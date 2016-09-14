9a0d78e1920e2a784952411b02778f7f8e9f02b1
Caleb Gibbs <calebgibbs@me.com>
2016-09-14 14:03:48 +1200

    finished contact page

--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){ 
-	$('#name').blur(validateName);
+	$('#name').blur(validateName); 
+	$('#phone').blur(validatePhone); 
+	$('#email').blur(validateEmail) 
+	$('#message').blur(validateMessage);
 });  
 
 var namePattern = new RegExp("^[a-zA-Z.' -]{2,50}$");  
@@ -11,15 +14,93 @@ function validateName(){
 	var nameValue = $(this).val(); 
 
 	if( nameValue.length === 0){
+		
 		$('#name-error').removeClass('success').addClass('error');
 		$('#name-error').html('This feild is required');
 		return;
-	} else if( namePattern.test(nameValue.value) ) {
+
+	} else if( ! namePattern.test(nameValue) ) {
+		
 		$('#name-error').removeClass('success').addClass('error');
 		$('#name-error').html('Please enter a valid name');
 		return;	
-	}else{ 
-		$('#name-error').removeClass('error').addClass('success');
-		$('#name-error').html('Good to go!');	
+
+	}else{ 	
+		$('#name-error').html('');
+		return;	
+	}
+} 
+
+function validatePhone() { 
+
+	var phoneValue = $(this).val(); 
+
+	if( phoneValue.length === 0){
+		
+		$('#phone-error').removeClass('success').addClass('error');
+		$('#phone-error').html('This feild is required');
+		return;
+
+	} else if( ! phonePattern.test(phoneValue) ) {
+		
+		$('#phone-error').removeClass('success').addClass('error');
+		$('#phone-error').html('Please enter a valid phone number');
+		return;	
+
+	}else{ 	
+		$('#phone-error').html('');
+		return;	
+	}
+} 
+
+function validateEmail() { 
+
+	var emailValue = $(this).val(); 
+
+	if( emailValue.length === 0){
+		
+		$('#email-error').removeClass('success').addClass('error');
+		$('#email-error').html('This feild is required');
+		return;
+
+	} else if( ! emailPattern.test(emailValue) ) {
+		
+		$('#email-error').removeClass('success').addClass('error');
+		$('#email-error').html('Please enter a valid email address');
+		return;	
+
+	}else{ 	
+		$('#email-error').html('');
+		return;	
 	}
 }
+
+function validateMessage() { 
+
+	var messageValue = $(this).val(); 
+
+	if( messageValue.length === 0){
+		
+		$('#message-error').removeClass('success').addClass('error');
+		$('#message-error').html('This feild is required');
+		return;
+
+	} else { 
+		$('#message-error').html('');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
